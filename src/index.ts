7842ac14aaf630bffcfff886441e2b72bcda3b69
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2025-03-28 15:23:57 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import sequelize from './config/database';
+import authRouter from './routes/auth.router'
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Sync database
-sequelize.sync({ force: false }) // Change to 'true' to drop and recreate tables
-  .then(() => console.log('Database connected'))
-  .catch((err) => console.error('Database connection error:', err));
 
 
 // Routes
@@ -18,7 +14,17 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello world!');
 });
 
+app.use('/api/users', authRouter)
+
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
+  // Sync database
+  sequelize.sync({ force: false }) // Change to 'true' to drop and recreate tables
+  .then(() => console.log('Database connected'))
+  .catch((err) => console.error('Database connection error:', err));
+
+
   console.log(`Server is running on port ${port}`);
 });
